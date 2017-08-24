b00837ef53ba0109c9a7543de677777ab4b0d74f
Jan Vereecken <jan.vereecken@gmail.com>
2017-08-24 21:13:46 +0200

    Fix test for deprecated function replacements

--- lib/bigflake/base62.ex
+++ lib/bigflake/base62.ex
@@ -1,15 +1,15 @@
 defmodule Bigflake.Base62 do
   @moduledoc false
-  
+
   def encode(data, opts \\ []) do
     len = Keyword.get(opts, :length)
     data |> Base62.encode |> with_padding(len)
   end
 
   def decode(data) do
-    data |> String.trim_leading(?0) |> Base62.decode!
+    data |> String.trim_leading("0") |> Base62.decode!
   end
 
   defp with_padding(data, nil), do: data
-  defp with_padding(data, len), do: String.pad_leading(data, len, ?0)
+  defp with_padding(data, len), do: String.pad_leading(data, len, "0")
 end
